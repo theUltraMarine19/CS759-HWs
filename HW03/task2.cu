c035918c274a7c3c54a6460f8a53c829a58cfd09
Arijit Pramanik <apramanik3@wisc.edu>
2020-02-12 09:06:42 -0600

    Clang-format not added in prev

--- HW03/task2.cu
+++ HW03/task2.cu
@@ -1,32 +1,32 @@
-#include<cstdio>
+#include <cstdio>
 #define N 16
 #define THREADS_PER_BLOCK 8
 using namespace std;
 
-__global__ void add(int* dA) {
-	int idx = threadIdx.x + blockIdx.x* blockDim.x;
-	dA[idx] = threadIdx.x + blockIdx.x;
+__global__ void add(int *dA) {
+  int idx = threadIdx.x + blockIdx.x * blockDim.x;
+  dA[idx] = threadIdx.x + blockIdx.x;
 }
 
 int main() {
-	int *dA;
-	int size = N*sizeof(int);
-	cudaMalloc((void**)&dA, size);
+  int *dA;
+  int size = N * sizeof(int);
+  cudaMalloc((void **)&dA, size);
 
-	int *hA;
-	hA = new int[size];
+  int *hA;
+  hA = new int[size];
 
-	add<<<2,THREADS_PER_BLOCK>>>(dA);
+  add<<<2, THREADS_PER_BLOCK>>>(dA);
 
-	cudaMemcpy(hA, dA, size, cudaMemcpyDeviceToHost);
+  cudaMemcpy(hA, dA, size, cudaMemcpyDeviceToHost);
 
-	for(int i = 0; i < N; i++)
-		if (i == N-1)
-			printf("%d", hA[i]);
-		else
-			printf("%d ", hA[i]);
+  for (int i = 0; i < N; i++)
+    if (i == N - 1)
+      printf("%d", hA[i]);
+    else
+      printf("%d ", hA[i]);
 
-	free(hA);
-	cudaFree(dA);
-	return 0;
+  free(hA);
+  cudaFree(dA);
+  return 0;
 }
--- HW03/task3.cu
+++ HW03/task3.cu
@@ -1,51 +1,52 @@
+#include <cstdio>
 #include "vadd.cuh"
-#include<cstdio>
 #define THREADS_PER_BLOCK 512
 using namespace std;
 
-int main(int argc, char* argv[]) {
-	int n = atoi(argv[1]);
+int main(int argc, char *argv[]) {
+  int n = atoi(argv[1]);
 
-	cudaEvent_t start;
-	cudaEvent_t stop;
-	cudaEventCreate(&start);
-	cudaEventCreate(&stop);
+  cudaEvent_t start;
+  cudaEvent_t stop;
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
 
-	float *ha, *hb;
-	ha = new float[n];
-	hb = new float[n];
+  float *ha, *hb;
+  ha = new float[n];
+  hb = new float[n];
 
-	for (int i = 0; i < n; i++) {
-		ha[i] = 1.0*(i+1);
-		hb[i] = 1.0*(i+1);
-	}
+  for (int i = 0; i < n; i++) {
+    ha[i] = 1.0 * (i + 1);
+    hb[i] = 1.0 * (i + 1);
+  }
 
-	float *da, *db;
-	cudaMalloc((void**)&da, n*sizeof(float));
-	cudaMalloc((void**)&db, n*sizeof(float));
+  float *da, *db;
+  cudaMalloc((void **)&da, n * sizeof(float));
+  cudaMalloc((void **)&db, n * sizeof(float));
 
-	cudaMemcpy(da, ha, n*sizeof(float), cudaMemcpyHostToDevice);
-	cudaMemcpy(db, hb, n*sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(da, ha, n * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(db, hb, n * sizeof(float), cudaMemcpyHostToDevice);
 
-	int num_blocks = (n-1)/THREADS_PER_BLOCK + 1;
+  int num_blocks = (n - 1) / THREADS_PER_BLOCK + 1;
 
-	cudaEventRecord(start);
-	vadd<<<num_blocks, THREADS_PER_BLOCK>>>(da, db, n);
-	cudaEventRecord(stop);
-	cudaEventSynchronize(stop);
+  cudaEventRecord(start);
+  vadd<<<num_blocks, THREADS_PER_BLOCK>>>(da, db, n);
+  cudaEventRecord(stop);
+  cudaEventSynchronize(stop);
 
-	float ms;
-	cudaEventElapsedTime(&ms, start, stop);
-	
-	printf("%f\n", ms/1000.0);
+  float ms;
+  cudaEventElapsedTime(&ms, start, stop);
 
-	cudaMemcpy(hb, db, n*sizeof(float), cudaMemcpyDeviceToHost);
+  printf("%f\n", ms / 1000.0);
 
-	printf("%f\n%f\n", hb[0], hb[n-1]);
+  cudaMemcpy(hb, db, n * sizeof(float), cudaMemcpyDeviceToHost);
 
-	free(ha); free(hb);
-	cudaFree(da); cudaFree(db);
+  printf("%f\n%f\n", hb[0], hb[n - 1]);
 
-	return 0;
+  free(ha);
+  free(hb);
+  cudaFree(da);
+  cudaFree(db);
 
+  return 0;
 }
