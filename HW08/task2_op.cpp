92e11cadc602c1e8040d7e95eacceedc4667ff24
Arijit Pramanik <apramanik3@wisc.edu>
2020-03-25 03:58:33 -0500

    Consult with TA

--- HW08/task2_op.cpp
+++ HW08/task2_op.cpp
@@ -1,3 +1,5 @@
+// #define _GNU_SOURCE
+
 #include <chrono>
 #include <cstdlib>
 #include <iostream>
@@ -6,6 +8,8 @@
 #include <omp.h>
 #include "convolution.h"
 
+#include <cstdio>
+#include <sched.h>
 using namespace std;
 
 int main(int argc, char* argv[]) {
@@ -20,21 +24,21 @@ int main(int argc, char* argv[]) {
   float* image = new float[n * n];
   float* output = new float[n * n];
   float* mask = new float[3 * 3];
+  
   omp_set_num_threads(t);
   
+  // Have a fraction of image on both NUMA node 0 and 1. schedule(static) is anyway default
   #pragma omp parallel for schedule(static)
   for (size_t i = 0; i < n * n; i++) {
     // image[i] = static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / 50.0));
     image[i] = 1.0;
   }
 
-  #pragma omp parallel
-  {
-    for (size_t i = 0; i < 3 * 3; i++) {
+  // Entire mask(36 bits) will get cached by resp threads in just one cross NUMA node access
+  for (size_t i = 0; i < 3 * 3; i++) {
     // mask[i] = static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / 10.0));
     mask[i] = 1.0;
-    }  
-  }
+  }  
   
 
   // float image[n*n] = {1, 3, 4, 8, 6, 5, 2, 4, 3, 4, 6, 8, 1, 4, 5, 2};
@@ -47,15 +51,16 @@ int main(int argc, char* argv[]) {
   // char env[] = "OMP_PROC_BIND=spread";
   // putenv(env);
   // cout << getenv("OMP_PROC_BIND") << endl;
-  // system("export OMP_PROC_BIND=close");
-  start = chrono::high_resolution_clock::now();
-  #pragma omp parallel proc_bind(close) 
-  {
-    #pragma omp master
-    cout << omp_get_proc_bind() << endl;
-    Convolve(image, output, n, mask, 3);
-  }
 
+  #pragma omp parallel
+  {
+    int thread_num = omp_get_thread_num();
+    int cpu_num = sched_getcpu();
+    printf("Thread %3d is running on CPU %3d\n", thread_num, cpu_num);
+  }
+  start = chrono::high_resolution_clock::now();
+  cout << omp_get_proc_bind() << endl;
+  Convolve(image, output, n, mask, 3);
   end = chrono::high_resolution_clock::now();
 
   duration_sec = chrono::duration_cast<chrono::duration<double, milli>>(end - start);
